2d54810cf23d2e4731897bb32016fe19896a35fe
nazem0_home_pc <mohamednazem09@gmail.com>
2024-03-28 18:41:04 +0200

    add property, without validations

--- src/app/components/admin/property-actions/property-actions.component.ts
+++ src/app/components/admin/property-actions/property-actions.component.ts
@@ -1,7 +1,7 @@
 import { GovernorateAndCityService } from './../../../api/services/governorate-and-city.service';
 import { PropertyForAdminService } from './../../../api/services/property-for-admin.service';
 import { Component, Input, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray, FormControl } from '@angular/forms';
 import { FacilityViewModelForAdmin, GovernorateAndCityViewModel, PropertyDetailsViewModelForUser } from '../../../api/models';
 import { BusinessHelper } from '../../../services/business-helper';
 import { Helper } from '../../../services/helper';
@@ -13,8 +13,9 @@ import { Helper } from '../../../services/helper';
 })
 export class PropertyActionsComponent implements OnInit {
   helper = Helper;
+  math=Math;
   propertyForm!: FormGroup;
-  propertyTypes = BusinessHelper.propertyTypes
+  propertyTypes = BusinessHelper.propertyTypes;
   text: string = `
   <h1>Title 1</h1>
   <ol>
@@ -34,10 +35,6 @@ export class PropertyActionsComponent implements OnInit {
   facilities: FacilityViewModelForAdmin[] = [];
   selectedFacility?: FacilityViewModelForAdmin;
   selectedFacilityValue?: string;
-  addedFacilities: {
-    id: number,
-    value: string
-  }[] = []
   governorates: GovernorateAndCityViewModel[] = [];
   cities: GovernorateAndCityViewModel[] = [];
   selectedGov?: GovernorateAndCityViewModel;
@@ -56,28 +53,28 @@ export class PropertyActionsComponent implements OnInit {
   }
   initPropertyForm() {
     this.propertyForm = this.fb.group({
-      location: '',
-      governorateId: 0,
-      cityId: 0,
-      unitPrice: 0,
-      description: '',
-      maintenanceCost: 0,
-      transactionFees: 0,
-      numberOfShares: 0,
-      sharePrice: 0,
-      annualRentalYield: 0,
-      annualPriceAppreciation: 0,
-      downPayment: 0,
-      monthlyInstallment: 0,
-      numberOfYears: 0,
-      maintenaceInstallment: 0,
-      deliveryInstallment: 0,
-      type: 1,
-      facilities: this.fb.array([])
+      location: new FormControl<string>('', [Validators.required]),
+      governorateId: new FormControl<number | undefined>(undefined, [Validators.required]),
+      cityId: new FormControl<number | undefined>(undefined, [Validators.required]),
+      description: new FormControl<string>(this.text, [Validators.required]),
+      unitPrice: new FormControl<number | undefined>(undefined, [Validators.required]),
+      maintenanceCost: new FormControl<number | undefined>(undefined, [Validators.required]),
+      transactionFees: new FormControl<number | undefined>(undefined, [Validators.required]),
+      numberOfShares: new FormControl<number | undefined>(undefined, [Validators.required]),
+      sharePrice: new FormControl<number | undefined>(undefined, [Validators.required]),
+      annualRentalYield: new FormControl<number | undefined>(undefined, [Validators.required]),
+      annualPriceAppreciation: new FormControl<number | undefined>(undefined, [Validators.required]),
+      downPayment: new FormControl<number | undefined>(undefined, [Validators.required]),
+      monthlyInstallment: new FormControl<number | undefined>(undefined, [Validators.required]),
+      numberOfYears: new FormControl<number | undefined>(undefined, [Validators.required]),
+      maintenaceInstallment: new FormControl<number | undefined>(undefined, [Validators.required]),
+      deliveryInstallment: new FormControl<number | undefined>(undefined, [Validators.required]),
+      type: new FormControl<number | undefined>(undefined, [Validators.required]),
+      facilities: new FormControl<FacilityViewModelForAdmin[]>([])
     });
   }
   check() {
-    console.log(this.text);
+    console.log(this.propertyForm.value);
 
   }
 
@@ -91,13 +88,14 @@ export class PropertyActionsComponent implements OnInit {
         }
       })
   }
-  initCity() {
+  initCity() {    
     this
       .governorateAndCityService
-      .apiCitiesGetByGovernorateIdGet$Json({ "GovernorateId": this.selectedGov?.id })
+      .apiCitiesGetByGovernorateIdGet$Json({ "GovernorateId": this.propertyForm.controls['governorateId'].value })
       .subscribe({
         next: next => {
           this.cities = next.data ?? []
+          if(this.cities.length) this.propertyForm.get('cityId')?.enable();
         }
       })
 
@@ -116,17 +114,32 @@ export class PropertyActionsComponent implements OnInit {
     return this.propertyForm.get('facilities') as FormArray;
   }
   createFacility() {
-    
+    return { facilityId: this.selectedFacility!.id!, description: this.selectedFacilityValue! }
   }
   addFacility(): void {
-    this.addedFacilities.push({ id: this.selectedFacility!.id!, value: this.selectedFacilityValue! })
+    this.facilitiesFormArray.value.push(this.createFacility());
     this.selectedFacility = undefined;
-    this.selectedFacilityValue="";
+    this.selectedFacilityValue = "";
+    console.log(this.propertyForm);
+
   }
   removeFacility(index: number): void {
-    this.addedFacilities.splice(index,1);
+    this.facilitiesFormArray.value.splice(index,1);
   }
-  fiterFacilities(id:number){
-    return this.facilities.find(e=>e.id == id)
+  fiterFacilities(id: number) {
+    return this.facilities.find(e => e.id == id)
+  }
+
+  setControllerValue(controlName:string ,value:unknown, percentage=false){
+    if(percentage) (value as number)*=0.01;
+    console.log(value);
+    this.propertyForm.controls[controlName].setValue(value);
+  }
+
+  submitForm(){
+    this
+    .propertyForAdminService
+    .apiDashboardPropertyAddPost$Json({body:this.propertyForm.value})
+    .subscribe();
   }
 }
