e1d5ac77dc0fb68a001e4454d468e979ecf27981
nazem0_home_pc <mohamednazem09@gmail.com>
2024-04-25 13:54:40 +0200

    orders pdf

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     api:"https://qisma.runasp.net",
     staticFiles:"https://qisma.runasp.net/Content/Images",
     Facilities:"https://qisma.runasp.net/Content/Facilities",
-    OrdersPdf:"https://qisma.runasp.net/Content/Orders_PDF/"
+    OrdersPdf:"https://qisma.runasp.net/Content/Orders_PDF"
 }
