e107bb7f6ea4a7b08bf1bb0f4c52b6347087f5f2
nazem0_home_pc <mohamednazem09@gmail.com>
2024-04-28 21:32:40 +0300

    removed unnecessary files

--- src/app/api$/services.ts
+++
@@ -1,6 +0,0 @@
-export { GovernorateAndCityService } from './services/governorate-and-city.service';
-export { OrderService } from './services/order.service';
-export { PropertyService } from './services/property.service';
-export { PropertyForAdminService } from './services/property-for-admin.service';
-export { SettingService } from './services/setting.service';
-export { UserAccountService } from './services/user-account.service';
