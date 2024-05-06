c1ea446acdd459cc2c5ac62aa687ac606aa575f0
nazem0_home_pc <mohamednazem09@gmail.com>
2024-05-06 17:47:53 +0300

    blog-card ui

--- src/app/components/shared/blog-card/blog-card.component.ts
+++ src/app/components/shared/blog-card/blog-card.component.ts
@@ -2,19 +2,21 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Blog } from '../../../api/models';
 import { Helper } from '../../../helpers/helper';
 import { ActivatedRoute, RouterModule } from '@angular/router';
+import { TooltipModule } from 'primeng/tooltip';
+import { TruncateTextPipe } from '../../../pipes/truncate-text.pipe';
 
 @Component({
   selector: 'app-blog-card',
   templateUrl: './blog-card.component.html',
   styleUrls: ['./blog-card.component.css'],
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, TooltipModule, TruncateTextPipe],
 })
 export class BlogCardComponent implements OnInit {
   @Input() blog?: Blog;
   isInAdminPanel : boolean;
   constructor(private route:ActivatedRoute) {
-    this.isInAdminPanel = route.snapshot.data['isAdmin']
+    this.isInAdminPanel = this.route.snapshot.data['isAdmin']
   }
 
   ngOnInit() {}
