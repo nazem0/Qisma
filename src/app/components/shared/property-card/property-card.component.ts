349d7560ee6a9400cb4cbf3d50ebd926ae238eec
nazem0_home_pc <mohamednazem09@gmail.com>
2024-05-11 21:00:53 +0300

    fixed property card percentage

--- src/app/components/shared/property-card/property-card.component.ts
+++ src/app/components/shared/property-card/property-card.component.ts
@@ -1,22 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { PropertyViewModelInListView } from '../../../api/models';
 import { Helper } from '../../../helpers/helper';
+import { PercentPipe } from '@angular/common';
 
 @Component({
   selector: 'app-property-card',
   templateUrl: './property-card.component.html',
   styleUrl: './property-card.component.css',
-  standalone:true
+  standalone:true,
+  imports:[PercentPipe]
 })
 export class PropertyCardComponent {
-  @Input() property:PropertyViewModelInListView;
+  @Input({required:true}) property!:PropertyViewModelInListView;
   appHelper = Helper;
-  constructor(){
-    this.property={
-      imageUrl:"/assets/page-resources/dummy-property.webp",
-      address:"110 Gardengate Dr",
-      city:"Harvest, Alabama 35749",
-      projectedAnnualReturn:5.7
-    }
-  }
+  constructor(){}
 }
